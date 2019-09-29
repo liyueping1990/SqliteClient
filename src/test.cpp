50736a82cb9cf8d2efcacbcca70e2bfa58b9847b
liyueping <liyueping@megvii.com>
2019-09-29 13:58:37 +0800

    fix

--- src/test.cpp
+++ src/test.cpp
@@ -3,13 +3,14 @@
 int main(int argc, char* argv[])
 {
 	SqliteClient::Ptr sqlite_client(new SqliteClient);
-	int result = sqlite_client->OpenDb("demodb");
+	int result = sqlite_client->OpenDb("../../gb_server.db");
 	if (result)
 	{
 		return result;
 	}
 
 	DataListPtr data_list;
-	sqlite_client->GetAllData("client", data_list);
+	sqlite_client->GetData("SELECT * FROM GB_platform WHERE platform_ID = 1", data_list);
+
 	return result;
 }
